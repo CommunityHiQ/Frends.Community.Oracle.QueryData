03cd9ce7a0bfde70834c8cfd0b3ea85ffdf34edb
Jura Laakkonen <jura.laakkonen@hiq.fi>
2017-12-07 09:22:20 +0200

    FCOM-110: - changed unit tests to use NUnit

--- Frends.Community.Oracle.QueryData.Tests/QueryDataTests.cs
+++ Frends.Community.Oracle.QueryData.Tests/QueryDataTests.cs
@@ -1,34 +1,31 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Frends.Community.Oracle.QueryData;
-using System.Threading;
+﻿using Frends.Community.Oracle.QueryData;
+using NUnit.Framework;
 
 namespace Frends.Community.Oracle.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
         string connectionString = "Data Source=localhost;User Id=<userid>;Password=<salasana>;Persist Security Info=True;";
 
-        [TestMethod]
+        [Test]
+        [Ignore("No way to automate this test without an Oracle instance")]
         public void QueryOracle()
         {
-            // No way to automate this test without an Oracle instance.So it's just commented out.
+            string query = "SELECT * FROM XPNIS_SYSTEM_SYSDESCR;";
 
-            //string query = "SELECT * FROM XPNIS_SYSTEM_SYSDESCR;";
+            Input Inputs = new Input();
 
-            //Input Inputs = new Input();
+            Inputs.Query = query;
+            Inputs.ConnectionString = connectionString;
 
-            //Inputs.Query = query;
-            //Inputs.ConnectionString = connectionString;
+            Inputs.MaxmimumRows = 100;
+            Inputs.RootElementName = "root";
+            Inputs.RowElementName = "row";
 
-            //Inputs.MaxmimumRows = 100;
-            //Inputs.RootElementName = "root";
-            //Inputs.RowElementName = "row";
+            var result = QueryData.QueryData.PerformQuery(Inputs);
 
-            //var result = QueryData.QueryData.PerformQuery(Inputs);
-
-            //Assert.AreEqual(System.Threading.Tasks.TaskStatus.RanToCompletion, result.Status);
+            Assert.AreEqual(System.Threading.Tasks.TaskStatus.RanToCompletion, result.Status);
         }
     }
 }
